f5931dce8b7b499024d7011c3fb6ce13286c3d7c
nsaripalli <nithinsaripalli@gmail.com>
2020-06-28 12:53:49 -0500

    added two mmore param metadata and reoriganized columns in output

--- type-signature/code-all.R
+++ type-signature/code-all.R
@@ -3,7 +3,7 @@ library(pryr)
 library(dplyr)
 library(stringr)
 
-call_arguments_data <- tibble(call_exit_id = numeric(0), fun_name = str(0), package_details=str(0), is_call_user = numeric(0), call_stack_depth = numeric(0), is_user_call_with_pipe_operator = numeric(0), param_index = numeric(0), caller = str(0), line_number = str(0), type_of_argument = str(0), class_of_argument = str(0), address_of_argument = character(0))
+call_arguments_data <- tibble(call_exit_id = numeric(0), fun_name = str(0), is_call_user = numeric(0), call_stack_depth = numeric(0), is_user_call_with_pipe_operator = numeric(0), caller = str(0), line_number = str(0), param_index = numeric(0), is_param_missing = numeric(1), name_of_param = str(0), type_of_argument = str(0), class_of_argument = str(0), address_of_argument = character(0), package_details = str(0))
 call_exit_data <- tibble(call_exit_id = numeric(0), fun_name = str(0), type_of_output = str(0), class_of_output = str(0), address_of_output = character(0))
 
 functions <- get_package_function_names("dplyr", public = TRUE, private = FALSE)
@@ -122,13 +122,16 @@ call_exit_callback <- function(context, application, package, func, call) {
 
   # Look at the model call stack to determine where the function was called from
   call_stack <- capture.output(get_call_stack(application))
-  call_stack_depth <- as.numeric(str_sub(c(call_stack[1]), 11, -9))
+  call_stack_depth <- get_size(get_call_stack(application))
   is_call_user <- call_stack_depth == 1
   package_details <- capture.output(package)
 
   for (parameter in parameters) {
 
     arguments <- get_arguments(parameter)
+
+    is_param_missing <- is_missing(parameter)
+    name_of_param <- get_name(parameter)
 
     if (is_vararg(parameter)) {
       type_sig <- merge_type(type_sig, index, "...")
@@ -144,7 +147,9 @@ call_exit_callback <- function(context, application, package, func, call) {
         class_of_argument = "vararg",
         call_stack_depth = call_stack_depth,
         is_call_user = is_call_user,
-        package_details = package_details
+        package_details = package_details,
+        is_param_missing = is_param_missing,
+        name_of_param = name_of_param,
       )
     }
     else if (length(arguments) == 0) {
@@ -161,7 +166,9 @@ call_exit_callback <- function(context, application, package, func, call) {
         class_of_argument = "No arguments",
         call_stack_depth = call_stack_depth,
         is_call_user = is_call_user,
-        package_details = package_details
+        package_details = package_details,
+        is_param_missing = is_param_missing,
+        name_of_param = name_of_param,
       )
     }
     else if (!is_evaluated(arguments[[1]])) {
@@ -179,7 +186,9 @@ call_exit_callback <- function(context, application, package, func, call) {
         address_of_argument = paste("Not evaluated, expression is: ", capture.output(get_expression(arguments[[1]]))),
         call_stack_depth = call_stack_depth,
         is_call_user = is_call_user,
-        package_details = package_details
+        package_details = package_details,
+        is_param_missing = is_param_missing,
+        name_of_param = name_of_param,
       )
     }
     else {
@@ -200,7 +209,9 @@ call_exit_callback <- function(context, application, package, func, call) {
         address_of_argument = address(argument_value),
         call_stack_depth = call_stack_depth,
         is_call_user = is_call_user,
-        package_details = package_details
+        package_details = package_details,
+        is_param_missing = is_param_missing,
+        name_of_param = name_of_param,
       )
     }
 
@@ -240,122 +251,124 @@ trace_code(context = context,
            code = {
              library(dplyr)
 
-             starwars %>%
-               filter(species == "Droid")
+             #starwars %>%
+             #  filter(species == "Droid")
 
-             starwars %>%
-               select(name, ends_with("color"))
+             filter(starwars, species == "Droid", .preserve = True)
 
-             starwars %>%
-               mutate(name, bmi = mass / ((height / 100)^2)) %>%
-               select(name:mass, bmi)
-
-             starwars %>%
-               arrange(desc(mass))
-
-             starwars %>%
-               group_by(species) %>%
-               summarise(
-                 n = n(),
-                 mass = mean(mass, na.rm = TRUE)
-               ) %>%
-               filter(n > 1)
-
-             starwars %>%
-               summarise(across(where(is.character), ~length(unique(.x))))
-
-             starwars %>%
-               group_by(species) %>%
-               filter(n() > 1) %>%
-               summarise(across(c(sex, gender, homeworld), ~length(unique(.x))))
-
-             starwars %>%
-               group_by(homeworld) %>%
-               filter(n() > 1) %>%
-               summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
-
-
-             df <- data.frame(g = c(1, 1, 2), x = c(-1, 1, 3), y = c(-1, -4, -9))
-             df %>%
-               group_by(g) %>%
-               summarise(across(where(is.numeric), sum))
-
-
-             min_max <- list(
-               min = ~min(.x, na.rm = TRUE),
-               max = ~max(.x, na.rm = TRUE)
-             )
-
-             starwars %>% summarise(across(where(is.numeric), min_max))
-
-
-             starwars %>% summarise(across(where(is.numeric), min_max, .names = "{fn}.{col}"))
-
-
-             starwars %>% summarise(
-               across(where(is.numeric), ~min(.x, na.rm = TRUE), .names = "min_{col}"),
-               across(where(is.numeric), ~max(.x, na.rm = TRUE), .names = "max_{col}")
-             )
-
-             df <- tibble(x = 1:3, y = 3:5, z = 5:7)
-             mult <- list(x = 1, y = 10, z = 100)
-             df %>% mutate(across(all_of(names(mult)), ~.x * mult[[cur_column()]]))
-
-             df <- data.frame(x = c(1, 2, 3), y = c(1, 4, 9))
-             df %>%
-               summarise(n = n(), across(where(is.numeric), sd))
-
-             df %>%
-               summarise(across(where(is.numeric), sd), n = n())
-
-
-             df %>%
-               summarise(n = n(), across(where(is.numeric) & !n, sd))
-
-             rescale01 <- function(x) {
-               rng <- range(x, na.rm = TRUE)
-               (x - rng[1]) / (rng[2] - rng[1])
-             }
-
-             df <- tibble(x = 1:4, y = rnorm(4))
-
-             df %>% mutate(across(where(is.numeric), rescale01))
-
-             starwars %>% filter(across(everything(), ~!is.na(.x)))
-
-
-             starwars %>% distinct(across(contains("color")))
-
-             starwars %>% count(across(contains("color")), sort = TRUE)
-
-             df %>% mutate_if(is.numeric, mean, na.rm = TRUE)
-             # ->
-             df %>% mutate(across(where(is.numeric), mean, na.rm = TRUE))
-             df %>% mutate_at(vars(c(x, starts_with("y"))), mean)
-             # ->
-
-             df %>% mutate(across(c(x, starts_with("y")), mean, na.rm = TRUE))
-             with_tracing_disabled({
-                                     df %>% mutate_all(mean)
-                                   })
-             # ->
-             df %>% mutate(across(everything(), mean))
-
-             df <- tibble(x = c("a", "b"), y = c(1, 1), z = c(-1, 1))
-             df %>% filter(across(where(is.numeric), ~.x > 0))
-
-             rowAny <- function(x) rowSums(x) > 0
-
-             df %>% filter(rowAny(across(where(is.numeric), ~.x > 0)))
-
-             df <- tibble(x = 2, y = 4, z = 8)
-             with_tracing_disabled({
-                                     with_tracing_enabled({
-                                                            df %>% mutate_all(~.x / y)
-                                                          })
-                                   })
-
-             df %>% mutate(across(everything(), ~.x / y))
+             #starwars %>%
+             #  select(name, ends_with("color"))
+             #
+             #starwars %>%
+             #  mutate(name, bmi = mass / ((height / 100)^2)) %>%
+             #  select(name:mass, bmi)
+             #
+             #starwars %>%
+             #  arrange(desc(mass))
+             #
+             #starwars %>%
+             #  group_by(species) %>%
+             #  summarise(
+             #    n = n(),
+             #    mass = mean(mass, na.rm = TRUE)
+             #  ) %>%
+             #  filter(n > 1)
+             #
+             #starwars %>%
+             #  summarise(across(where(is.character), ~length(unique(.x))))
+             #
+             #starwars %>%
+             #  group_by(species) %>%
+             #  filter(n() > 1) %>%
+             #  summarise(across(c(sex, gender, homeworld), ~length(unique(.x))))
+             #
+             #starwars %>%
+             #  group_by(homeworld) %>%
+             #  filter(n() > 1) %>%
+             #  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE)))
+             #
+             #
+             #df <- data.frame(g = c(1, 1, 2), x = c(-1, 1, 3), y = c(-1, -4, -9))
+             #df %>%
+             #  group_by(g) %>%
+             #  summarise(across(where(is.numeric), sum))
+             #
+             #
+             #min_max <- list(
+             #  min = ~min(.x, na.rm = TRUE),
+             #  max = ~max(.x, na.rm = TRUE)
+             #)
+             #
+             #starwars %>% summarise(across(where(is.numeric), min_max))
+             #
+             #
+             #starwars %>% summarise(across(where(is.numeric), min_max, .names = "{fn}.{col}"))
+             #
+             #
+             #starwars %>% summarise(
+             #  across(where(is.numeric), ~min(.x, na.rm = TRUE), .names = "min_{col}"),
+             #  across(where(is.numeric), ~max(.x, na.rm = TRUE), .names = "max_{col}")
+             #)
+             #
+             #df <- tibble(x = 1:3, y = 3:5, z = 5:7)
+             #mult <- list(x = 1, y = 10, z = 100)
+             #df %>% mutate(across(all_of(names(mult)), ~.x * mult[[cur_column()]]))
+             #
+             #df <- data.frame(x = c(1, 2, 3), y = c(1, 4, 9))
+             #df %>%
+             #  summarise(n = n(), across(where(is.numeric), sd))
+             #
+             #df %>%
+             #  summarise(across(where(is.numeric), sd), n = n())
+             #
+             #
+             #df %>%
+             #  summarise(n = n(), across(where(is.numeric) & !n, sd))
+             #
+             #rescale01 <- function(x) {
+             #  rng <- range(x, na.rm = TRUE)
+             #  (x - rng[1]) / (rng[2] - rng[1])
+             #}
+             #
+             #df <- tibble(x = 1:4, y = rnorm(4))
+             #
+             #df %>% mutate(across(where(is.numeric), rescale01))
+             #
+             #starwars %>% filter(across(everything(), ~!is.na(.x)))
+             #
+             #
+             #starwars %>% distinct(across(contains("color")))
+             #
+             #starwars %>% count(across(contains("color")), sort = TRUE)
+             #
+             #df %>% mutate_if(is.numeric, mean, na.rm = TRUE)
+             ## ->
+             #df %>% mutate(across(where(is.numeric), mean, na.rm = TRUE))
+             #df %>% mutate_at(vars(c(x, starts_with("y"))), mean)
+             ## ->
+             #
+             #df %>% mutate(across(c(x, starts_with("y")), mean, na.rm = TRUE))
+             #with_tracing_disabled({
+             #                        df %>% mutate_all(mean)
+             #                      })
+             ## ->
+             #df %>% mutate(across(everything(), mean))
+             #
+             #df <- tibble(x = c("a", "b"), y = c(1, 1), z = c(-1, 1))
+             #df %>% filter(across(where(is.numeric), ~.x > 0))
+             #
+             #rowAny <- function(x) rowSums(x) > 0
+             #
+             #df %>% filter(rowAny(across(where(is.numeric), ~.x > 0)))
+             #
+             #df <- tibble(x = 2, y = 4, z = 8)
+             #with_tracing_disabled({
+             #                        with_tracing_enabled({
+             #                                               df %>% mutate_all(~.x / y)
+             #                                             })
+             #                      })
+             #
+             #df %>% mutate(across(everything(), ~.x / y))
            })
 
 call_arguments_data %>% write.csv(., "call_arguments_data.csv")
